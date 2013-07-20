988c9cd9656b9249210ea4e6f2c02d0013b27c45
alfredo.morresi <alfredo.morresi@gmail.com>
2013-07-20 14:24:11 +0200

    First project release

---
+++ 130720-hackitaly/TechnogymAssistant/src/it/rainbowbreeze/hackitaly13/IntroActivity.java
@@ -0,0 +1,25 @@
+package it.rainbowbreeze.hackitaly13;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class IntroActivity extends Activity
+{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.act_intro);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu)
+	{
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.intro, menu);
+		return true;
+	}
+
+}
