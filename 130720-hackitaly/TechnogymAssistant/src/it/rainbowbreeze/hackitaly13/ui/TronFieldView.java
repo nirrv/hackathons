496711a729feb7c3088528c1f275fca626148399
alfredo.morresi <alfredo.morresi@gmail.com>
2013-07-21 04:46:48 +0200

    added game screen

---
+++ 130720-hackitaly/TechnogymAssistant/src/it/rainbowbreeze/hackitaly13/ui/TronFieldView.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+package it.rainbowbreeze.hackitaly13.ui;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * @author alfredomorresi
+ *
+ */
+public class TronFieldView extends View {
+
+    public TronFieldView(Context context) {
+        super(context);
+    }
+
+    public TronFieldView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public TronFieldView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+    
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+    }
+
+}
